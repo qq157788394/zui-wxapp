981880d2f9f8af8d6f9c1de6f94a66e05c9ce049
huangzhuo <157788394@qq.com>
2018-08-28 20:08:27 +0800

    tag

---
+++ examples/dist/z-tag/index.js
@@ -0,0 +1,24 @@
+Component({
+  properties: {
+    size: {
+      type: String,
+      value: 'medium'
+    },
+    type: {
+      type: String,
+      value: 'primary'
+    }
+  },
+  data: {},
+  methods: {},
+  behaviors: {},
+  created: function created() {},
+  attached: function attached() {},
+  ready: function ready() {},
+  moved: function moved() {},
+  detached: function detached() {},
+
+  relations: {},
+  externalClasses: ['class-name'],
+  options: {}
+});
---
+++ src/z-tag/index.js
@@ -0,0 +1,23 @@
+Component({
+  properties: {
+    size: {
+      type: String,
+      value: 'medium'
+    },
+    type: {
+      type: String,
+      value: 'primary'
+    }
+  },
+  data: {},
+  methods: {},
+  behaviors: {},
+  created () {},
+  attached () {},
+  ready () {},
+  moved () {},
+  detached () {},
+  relations: {},
+  externalClasses: ['class-name'],
+  options: {}
+})
