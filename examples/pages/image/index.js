c6b3b943a20655ead3acb10943b3059de242ad85
huangzhuo <157788394@qq.com>
2018-09-18 20:48:54 +0800

    image

---
+++ examples/pages/image/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {},
+  onLoad () {},
+  onReady () {},
+  onShow () {},
+  onHide () {},
+  onUnload () {},
+  onPullDownRefresh () {},
+  onReachBottom () {},
+  onShareAppMessage () {},
+  onPageScroll () {},
+  onTabItemTap () {}
+})
