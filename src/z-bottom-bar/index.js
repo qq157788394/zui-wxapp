7a1c6e1f46ce104e481706b164c8b88c2e747537
huangzhuo <157788394@qq.com>
2018-09-18 17:43:00 +0800

    bottom-bar

---
+++ src/z-bottom-bar/index.js
@@ -0,0 +1,22 @@
+Component({
+  properties: {
+    divider: Boolean,
+    boxShadow: Boolean,
+    size: String
+  },
+  data: {},
+  methods: {},
+  behaviors: [],
+  relations: {},
+  externalClasses: [],
+  options: {},
+  lifetimes: {
+    created () {},
+    attached () {},
+    ready () {},
+    moved () {},
+    detached () {}
+  },
+  pageLifetimes: {},
+  definitionFilter () {}
+})
