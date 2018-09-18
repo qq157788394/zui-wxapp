10aabe764dab4446a8aa6bff1655993dd2bc80e3
huangzhuo <157788394@qq.com>
2018-09-18 17:45:00 +0800

    补充注释

--- src/z-bottom-bar/index.js
+++ src/z-bottom-bar/index.js
@@ -1,3 +1,12 @@
+/**
+ * z-bottom-bar 置底操作栏
+ * @desc 置底操作栏
+ *
+ * @prop { Boolean } divider - 顶部分割线
+ * @prop { Boolean } boxShadow - 阴影
+ * @prop { String } size - 尺寸，可选值medium, small, 默认值medium
+ */
+
 Component({
   properties: {
     divider: Boolean,
