84e7c52ee14cbe40c91186145c58ce5ff2a1c057
huangzhuo <157788394@qq.com>
2018-09-21 14:15:19 +0800

    panel-good创建

---
+++ examples/dist/z-panel-good/index.js
@@ -0,0 +1,18 @@
+Component({
+  properties: {},
+  data: {},
+  methods: {},
+  behaviors: [],
+  relations: {},
+  externalClasses: [],
+  options: {},
+  lifetimes: {
+    created () {},
+    attached () {},
+    ready () {},
+    moved () {},
+    detached () {}
+  },
+  pageLifetimes: {},
+  definitionFilter () {}
+})
